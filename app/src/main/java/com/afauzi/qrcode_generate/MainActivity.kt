73f4ffed82a3372661ba2d576930c97a461bd623
Akhmad Fauzi <akhmadfauzi0101@gmail.com>
2023-03-25 15:12:48 +0700

    update

--- app/src/main/java/com/afauzi/qrcode_generate/MainActivity.kt
+++ app/src/main/java/com/afauzi/qrcode_generate/MainActivity.kt
@@ -12,7 +12,13 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
+import androidx.core.content.ContextCompat
 import com.afauzi.qrcode_generate.databinding.ActivityMainBinding
+import com.github.alexzhirkevich.customqrgenerator.QrData
+import com.github.alexzhirkevich.customqrgenerator.style.Color
+import com.github.alexzhirkevich.customqrgenerator.vector.QrCodeDrawable
+import com.github.alexzhirkevich.customqrgenerator.vector.createQrVectorOptions
+import com.github.alexzhirkevich.customqrgenerator.vector.style.*
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.qrcode.QRCodeWriter
@@ -28,13 +34,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnGenerateQrCode.setOnClickListener {
-            val text = binding.etData.text.toString().trim()
-            val encoder = QRGEncoder(text, null, QRGContents.Type.TEXT, 800)
-            encoder.colorBlack = Color.WHITE
-            encoder.colorWhite = Color.BLACK
+
             try {
-                binding.tvHelperVisibility.visibility = View.GONE
-                binding.ivQrCode.setImageBitmap(encoder.bitmap)
+                val text = binding.etData.text.toString().trim()
+                val data = QrData.Url(text)
+                val option = createQrVectorOptions {
+                    padding = .125F
+                    logo {
+                        drawable = ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_launcher_background)
+                        size = .25f
+                        padding = QrVectorLogoPadding.Natural(.2f)
+                        shape = QrVectorLogoShape.Circle
+                    }
+                    colors {
+                        dark = QrVectorColor
+                            .Solid(Color(0xff345288))
+                        ball = QrVectorColor.Solid(
+                            ContextCompat.getColor(this@MainActivity, R.color.teal_700)
+                        )
+                    }
+                    shapes {
+                        darkPixel = QrVectorPixelShape
+                            .RoundCorners(.5f)
+                        ball = QrVectorBallShape
+                            .RoundCorners(.25f)
+                        frame = QrVectorFrameShape
+                            .RoundCorners(.25f)
+                    }
+                }
+                val drawable = QrCodeDrawable(data, option)
+                binding.ivQrCode.setImageDrawable(drawable)
             }catch (e: WriterException) {
                 binding.tvHelperVisibility.text = "Error generate qrcode"
             }
